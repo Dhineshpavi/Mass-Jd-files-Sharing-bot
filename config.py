b2be8ecce8416b5cbf127996cefd4f9d5e1fcc45
Dp_Botz <149217129+Dhineshpavi@users.noreply.github.com>
2024-04-18 08:40:30 -0700

    Update config.py

--- config.py
+++ config.py
@@ -28,7 +28,7 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "1357978966"))
 PORT = os.environ.get("PORT", "8080")
 
 #Database 
-DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://jdofficial143:<jayaraman143>@cluster0.b7w8f3d.mongodb.net/?retryWrites=true&w=majority")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://Aloneboy:Aloneboytg@cluster0.nmeelsr.mongodb.net/?retryWrites=true&w=majority")
 DB_NAME = os.environ.get("DATABASE_NAME", "DP_BOTZ")
 
 #force sub channel id, if you want enable force sub
