4a673bd339ac6bbfea3e4aaccc53447d163ba1f1
Dp_Botz <149217129+Dhineshpavi@users.noreply.github.com>
2024-05-28 01:00:58 -0700

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -93,8 +93,9 @@ async def start_command(client: Client, message: Message):
                 ]
             ]
         )
-        await message.reply_text(
-            text = START_MSG.format(
+        await message.reply_photo(
+            photo="https://graph.org/file/13e6563390704498e7a21.jpg",
+            caption = START_MSG.format(
                 first = message.from_user.first_name,
                 last = message.from_user.last_name,
                 username = None if not message.from_user.username else '@' + message.from_user.username,
@@ -102,7 +103,7 @@ async def start_command(client: Client, message: Message):
                 id = message.from_user.id
             ),
             reply_markup = reply_markup,
-            disable_web_page_preview = True,
+            # disable_web_page_preview = True,
             quote = True
         )
         return
